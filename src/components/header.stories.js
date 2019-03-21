a30fb48d158bd8cf51a6d7b12b1ea5ad1e0e4599
Dane Summers <dsummersl@yahoo.com>
2019-03-21 03:20:08 -0400

    Adds storybook support.

---
+++ src/components/header.stories.js
@@ -0,0 +1,7 @@
+import React from "react"
+import { storiesOf } from "@storybook/react"
+import Header from "./header"
+
+storiesOf(`Framework/Header`, module).add(`default`, () => (
+  <Header/>
+))
