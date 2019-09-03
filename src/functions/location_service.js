16b2bd6a7fdb0d03be65e32e61f95f828c41f0af
Dane Summers <dsummersl@yahoo.com>
2019-09-02 21:21:29 -0400

    Start functions.

--- src/functions/location_service.js
+++ src/functions/location_service.js
@@ -1,4 +1,7 @@
+import axios from 'axios';
+
 export default function(event, context, callback) {
+  // let info = await LocationService.lookup(location.location.lat, location.location.lng);
   callback(null, {
     statusCode: 200,
     body: "Hello, world!"
