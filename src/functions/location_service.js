3905b9680cc57b584b00b158963615dbbf7b16ad
Dane Summers <dsummersl@yahoo.com>
2019-09-02 21:25:53 -0400

    Name netlify function properly.

--- src/functions/location_service.js
+++ src/functions/location_service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export default function(event, context, callback) {
+export default function handler(event, context, callback) {
   // let info = await LocationService.lookup(location.location.lat, location.location.lng);
   callback(null, {
     statusCode: 200,
