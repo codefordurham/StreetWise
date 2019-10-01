f3bd1ea472da93f91385d4fa20dc8609a4002c03
Dane Summers <dsummersl@yahoo.com>
2019-10-01 19:29:30 -0400

    Adds work in progress Ward lookup via geojson

--- query_processor_app/find_polling_place.py
+++ query_processor_app/find_polling_place.py
@@ -2,6 +2,9 @@ import re
 import robobrowser
 from robobrowser import RoboBrowser
 from .formatting import formatAddress
+import geopandas
+from shapely.geometry import Point
+
 
 def findPollingPlace(address, zip_code):
     br = RoboBrowser()
@@ -18,3 +21,14 @@ def findPollingPlace(address, zip_code):
     polling_address_copy = polling_address
     polling_address = formatAddress(polling_address_copy)
     return polling_address
+
+def findWard(address, zip_code):
+    # TODO how do I get the lat/lng from the address and zip_code?
+    p = Point(-78.899690, 36.022480)
+    ward_data = geopandas.read_file('public/data/wards.geojson')
+
+    matching_layers = [i for i, val in enumerate(ward_data.intersects(p)) if val]
+    if len(matching_layers) == 0:
+        return 'nothing'
+
+    return ward_data.iloc[matching_layers[0]].Ward
