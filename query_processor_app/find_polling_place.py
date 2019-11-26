6907650f5ff612589ccfdbfcbd1908e5367611b9
ssciere <steve.sciere@gmail.com>
2019-11-26 14:32:21 -0500

    'more'

--- query_processor_app/find_polling_place.py
+++ query_processor_app/find_polling_place.py
@@ -39,11 +39,11 @@ def findWard(address, zip_code, lat, lon):
 
         matching_layers = [i for i, val in enumerate(ward_data.intersects(p)) if val]
         if len(matching_layers) == 0:
-            return 'n/a'
+            return 'Ward n/a'
         
         return ward_data.iloc[matching_layers[0]].Ward
     except:
-        return 'n/a'
+        return 'Ward n/a'
 
 def findPoliceDistrict(address, zip_code, lat, lon):
     try:  #try/except avoids throwing error if local developer does not have geopandas
