ca0750876714dff7b87cf79cc070c9e6908a007b
ssciere <steve.sciere@gmail.com>
2019-11-27 12:20:41 -0500

    added police dist lookup

--- streetwise/settings.py
+++ streetwise/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+#secret key
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
