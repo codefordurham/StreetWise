23fcd0ace4dbc4503011fd286ab627a1f0332fee
Dane Summers <dsummersl@yahoo.com>
2020-02-23 16:47:59 -0500

    Basic styling for key/value pairs on page.

--- src/Sheet.js
+++ src/Sheet.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import styles from './Sheet.module.css';
-import utilitiesSvg from './images/icon-transportation.svg';
+import SheetRegion from './SheetRegion';
+import transportationSvg from './images/icon-transportation.svg';
+import governmentSvg from './images/icon-government.svg';
+import homeSvg from './images/icon-home.svg';
+import safetySvg from './images/icon-health-and-safety.svg';
+import educationSvg from './images/icon-education.svg';
 
 function Sheet(props) {
   const { address, data } = props;
@@ -18,81 +23,87 @@ function Sheet(props) {
   return (
     <div className={styles.container}>
       <div className={styles.utilities}>
-        <div className={styles.address}>
-          <h4>{address}</h4>
-        </div>
+        <SheetRegion title={address} icon={homeSvg}>
+          <div className={styles.utilitiesWrapper}>
+            <dl className={styles.left}>
+              <dt>Electric</dt>
+              <dd><a href={data.electCompURL} target="_blank" rel="noopener noreferrer">Duke Energy</a> {data.electCompPhone}</dd>
+              <dt>Water</dt>
+              <dd><a href={data.waterURL} target="_blank" rel="noopener noreferrer">Durham Water Connection</a> {data.waterPhone} or
+                in person at {data.waterAddress}</dd>
+              <dt>Gas</dt>
+              <dd><a href={data.gasCompURL} target="_blank" rel="noopener noreferrer">{data.gasCompName}</a> {data.gasCompPhone}</dd>
+              <dt>Internet</dt>
+              <dd><a href={data.internetURL} target="_blank" rel="noopener noreferrer">{data.internetLookupSite}</a></dd>
+              <dt>TV</dt>
+              <dd><a href={data.TVURL} target="_blank" rel="noopener noreferrer">{data.TVLookupSite}</a></dd>
+            </dl>
 
-        <ul className={styles.left}>
-          <li><label>Electric </label><a href={data.electCompURL} target="_blank" rel="noopener noreferrer">Duke Energy</a> {data.electCompPhone}</li>
-          <li><label>Water </label> <a href={data.waterURL} target="_blank" rel="noopener noreferrer">Durham Water Connection</a> {data.waterPhone} or
-            in person at {data.waterAddress}</li>
-          <li><label>Gas </label> <a href={data.gasCompURL} target="_blank" rel="noopener noreferrer">{data.gasCompName}</a> {data.gasCompPhone}</li>
-          <li><label>Internet </label> <a href={data.internetURL} target="_blank" rel="noopener noreferrer">{data.internetLookupSite}</a></li>
-          <li><label>TV </label> <a href={data.TVURL} target="_blank" rel="noopener noreferrer">{data.TVLookupSite}</a></li>
-        </ul>
-
-        <ul className={styles.right}>
-          <li><label>Trash </label> {data.trashDays}</li>
-          <li><label>Recycling </label> {data.recyclingDays}</li>
-          <li><label>Nearest Park </label> {data.nearestPark}</li>
-          <li><label>Nearest Libraries </label> {data.nearestLibrary}</li>
-        </ul>
+            <dl className={styles.right}>
+              <dt>Trash </dt>
+              <dd>{data.trashDays}</dd>
+              <dt>Recycling </dt>
+              <dd>{data.recyclingDays}</dd>
+              <dt>Nearest Park </dt>
+              <dd>{data.nearestPark}</dd>
+              <dt>Nearest Libraries </dt>
+              <dd>{data.nearestLibrary}</dd>
+            </dl>
+          </div>
+        </SheetRegion>
       </div>
+
       <div className={styles.health}>
-        <h4>Health and Safety</h4>
-
-        <ul>
-          <li><label>Fire Dept </label> {data.nearestFireDept}</li>
-        </ul>
-
-        <ul>
-          <li><label>Police District </label>{data.policeDistrict}</li>
-        </ul>
-
-        <ul>
-          <li>
-            <label>
-              Neighborhood Listserv/watch
-            </label>{data.neighborhoodListServ}
-          </li>
-          <li><label>Nearest Hospital/ER/Clinic </label>{data.nearestHospital}</li>
-          <li><label>Emergency Alerts </label><a href={data.emergencyAlertsURL}
-              target="_blank" rel="noopener noreferrer">{data.emergencyAlertsWebsiteName}</a></li>
-        </ul>
-
+        <SheetRegion title="Health and Safety" icon={safetySvg}>
+          <dl>
+            <dt>Fire Dept </dt>
+            <dd>{data.nearestFireDept}</dd>
+            <dt>Police District </dt>
+            <dd>{data.policeDistrict}</dd>
+            <dt>Neighborhood Listserv/watch </dt>
+            <dd>{data.neighborhoodListServ}</dd>
+            <dt>Nearest Hospital/ER/Clinic </dt>
+            <dd>{data.nearestHospital}</dd>
+            <dt>Emergency Alerts </dt>
+            <dd><a href={data.emergencyAlertsURL}
+                target="_blank" rel="noopener noreferrer">{data.emergencyAlertsWebsiteName}</a></dd>
+          </dl>
+        </SheetRegion>
       </div>
 
       <div className={styles.government}>
-        <h4>Government</h4>
-
-        <ul>
-          <li><label>Poling location </label></li>
-          <li> {data.polingLocation} </li>
-
-        </ul>
-        <ul>
-          <li><label></label>{data.ward}</li>
-          <li><label>Durham County Board of Elections </label>{data.boardOfElectPhone}</li>
-        </ul>
-
+        <SheetRegion title="Government" icon={governmentSvg}>
+          <dl>
+            <dt>Polling location </dt>
+            <dd> {data.polingLocation} </dd>
+            <dt>Ward</dt>
+            <dd>{data.ward}</dd>
+            <dt>Durham County Board of Elections </dt>
+            <dd>{data.boardOfElectPhone}</dd>
+          </dl>
+        </SheetRegion>
       </div>
 
       <div className={styles.transportation}>
-        <h4>Transportation</h4>
-
-        <ul>
-          <li><label>Find a Taxi </label>{data.taxiInfo}</li>
-          <li><label>Find a Bus </label><a href={data.busURL} target="_blank" rel="noopener noreferrer">{data.busWebsiteName}</a></li>
-        </ul>
-
+        <SheetRegion title="Transportation" icon={transportationSvg}>
+          <dl>
+            <dt>Find a Taxi </dt>
+            <dd>{data.taxiInfo}</dd>
+            <dt>Find a Bus </dt>
+            <dd><a href={data.busURL} target="_blank" rel="noopener noreferrer">{data.busWebsiteName}</a></dd>
+          </dl>
+        </SheetRegion>
       </div>
 
       <div className={styles.education}>
-        <h4>Education</h4>
-        <ul>
-          <li><label>Durham School District </label>{data.schoolDistrict}</li>
-          <li><label>School Board </label> {data.schoolBoardInfo}</li>
-        </ul>
+        <SheetRegion title="Education" icon={educationSvg}>
+          <dl>
+            <dt>Durham School District </dt>
+            <dd>{data.schoolDistrict}</dd>
+            <dt>School Board </dt>
+            <dd>{data.schoolBoardInfo}</dd>
+          </dl>
+        </SheetRegion>
       </div>
     </div>
   );
